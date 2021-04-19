7003c6f41e4b353b9bf9f6b17a119ca9cb1917bf
aldoglielmo <ealdoglielmo@gmail.com>
2021-04-19 17:14:30 +0200

    improvements to Data_sets class. Firt attempt to serialize all computations contained in the Data class without copying and pasting the same code

--- duly/data_sets.py
+++ duly/data_sets.py
@@ -1,8 +1,10 @@
+import numpy as np
 from duly.data import Data
+
 
 class Data_sets:
 
-    def __init__(self, coordinates_list=(), distances_list=(), labels_list=(),
+    def __init__(self, coordinates_list=(), distances_list=(),
                  maxk_list=[None], verbose=False, njobs=1):
         if len(distances_list) == 0:
             self.Nsets = len(coordinates_list)
@@ -19,20 +21,13 @@ class Data_sets:
         if len(maxk_list) == 1:
             maxk_list = [maxk_list[0]] * self.Nsets
 
-        assert (len(labels_list) == 0 or len(labels_list) == self.Nsets)
-
         self.data_sets = []
 
         for i in range(self.Nsets):
             X = coordinates_list[i]
             dists = distances_list[i]
             maxk = maxk_list[i]
-            if len(labels_list) == 0:
-                data = Data(coordinates=X, distances=dists, maxk=maxk, verbose=verbose, njobs=njobs)
-            else:
-                labels = labels_list[i]
-                data = Data_wlabel(coordinates=X, distances=dists, labels=labels, maxk=maxk,
-                                   verbose=verbose, njobs=njobs)
+            data = Data(coordinates=X, distances=dists, maxk=maxk, verbose=verbose, njobs=njobs)
 
             self.data_sets.append(data)
 
@@ -48,12 +43,8 @@ class Data_sets:
 
     def add_one_dataset(self, coordinates=None, distances=None, labels=None, maxk=None):
 
-        if labels is None:
-            data = Data(coordinates=coordinates, distances=distances, maxk=maxk,
-                        verbose=self.verbose, njobs=self.njobs)
-        else:
-            data = Data_wlabel(coordinates=coordinates, distances=distances, labels=labels,
-                               maxk=maxk, verbose=self.verbose, njobs=self.njobs)
+        data = Data(coordinates=coordinates, distances=distances, maxk=maxk,
+                    verbose=self.verbose, njobs=self.njobs)
 
         self.data_sets.append(data)
         self.Nsets += 1
@@ -66,9 +57,10 @@ class Data_sets:
         for d in self.data_sets:
             d.gt_labels = labels
 
-    def compute_id_2NN(self, decimation= 1, fraction=0.9, n_reps = 1):
+    def compute_id_2NN(self, decimation=1, fraction=0.9, n_reps=1):
 
-        print('computing id: fraction = {}, decimation = {}, repetitions = {}, range = {}'.format(fraction, decimation, n_reps, 2))
+        print('computing id: fraction = {}, decimation = {}, repetitions = {}, range = {}'.format(fraction, decimation,
+                                                                                                  n_reps, 2))
         for i, d in enumerate(self.data_sets):
             print('computing id of dataset ', i)
             d.compute_id_2NN(decimation=decimation, fraction=fraction)
@@ -84,8 +76,21 @@ class Data_sets:
 
         self.ids = [d.ids_scaling[0] for d in self.data_sets]
 
-
+    def serialize_computation(self, computation_string, **kwargs):
+        """This method applies the computation defined in 'computation_string' to all Data instances
+        contained in the class.
+        """
+        for i, d in enumerate(self.data_sets):
+            comput = getattr(d, computation_string)
+            comput(**kwargs)
 
 
 if __name__ == '__main__':
-    pass
+    X1 = np.random.uniform(0, 1, (100, 2))
+    X2 = np.random.uniform(0, 1, (100, 2))
+
+    ds = Data_sets(coordinates_list=([X1, X2]))
+
+    ds.serialize_computation('compute_distances', maxk=3)
+
+    print(ds.data_sets[0].distances)
