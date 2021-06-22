2e781ef5a77821b517e386fa00df1c0771c2554e
aldoglielmo <ealdoglielmo@gmail.com>
2021-06-22 17:03:35 +0200

    removed old cython functions module and small things

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,25 +12,26 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../duly'))
-sys.path.insert(0, os.path.abspath('../../duly'))
 
-sys.path.insert(0, os.path.abspath('../cython_'))
-sys.path.insert(0, os.path.abspath('../../duly/cython_'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../duly"))
+sys.path.insert(0, os.path.abspath("../../duly"))
 
-sys.path.insert(0, os.path.abspath('../examples'))
-sys.path.insert(0, os.path.abspath('../../examples'))
+sys.path.insert(0, os.path.abspath("../cython_"))
+sys.path.insert(0, os.path.abspath("../../duly/cython_"))
+
+sys.path.insert(0, os.path.abspath("../examples"))
+sys.path.insert(0, os.path.abspath("../../examples"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'duly'
-copyright = '2021, authors'
+project = "duly"
+copyright = "2021, Glielmo et al."
 author = "Aldo Glielmo, Matteo Carli, Diego Doimo, Iuri Macocco, Maria D'Errico, Alex Rodrriguez, Alessandro Laio"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -38,15 +39,15 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'nbsphinx', 'nbsphinx_link', 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "nbsphinx", "nbsphinx_link", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['**.ipynb_checkpoints']
+exclude_patterns = ["**.ipynb_checkpoints"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -54,12 +55,20 @@ exclude_patterns = ['**.ipynb_checkpoints']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
-autodoc_mock_imports = ["duly.cython_.cython_functions", "cython_", "cython_functions"]
+autodoc_mock_imports = [
+    "cython_",
+    "cython_clustering",
+    "cython_maximum_likelihood_opt",
+    "cython_grads",
+    "duly.cython_clustering",
+    "duly.cython_maximum_likelihood_opt",
+    "duly.cython_grads",
+]
--- docs/source/utils.rst
+++ docs/source/utils.rst
@@ -1,7 +1,7 @@
 The utils module
 ==========================
 
-The utils module contains a variety of functions useful to the different classes of the package.
+The *utils* module contains a variety of functions useful to the different classes of the package.
 
 .. automodule:: utils
     :members:
