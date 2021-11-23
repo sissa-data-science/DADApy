54794b19069f19f8f76608ec5c23156e48a71c01
Diego <>
2021-11-23 18:44:08 +0100

    u

--- setup.py
+++ setup.py
@@ -1,93 +1,110 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
 
 cmdclass = {}
-ext_modules = cythonize("dadapy/cython_/*.pyx")
+ext_modules = []
+
+
+### COMPILE FROM C ###
+
+
+class get_numpy_include(object):
+    """Defer numpy.get_include() until after numpy is installed.
+    From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+    """
+
+    def __str__(self):
+        import numpy
+
+        return numpy.get_include()
+
+
+# ext_modules += [Extension("dadapy.cython_.cython_functions", sources=["dadapy/cython_/cython_functions.c"],
+#                           include_dirs=[get_numpy_include()])]
+
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_clustering",
+        sources=["dadapy/cython_/cython_clustering.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
+
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_grads",
+        sources=["dadapy/cython_/cython_grads.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
+
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_maximum_likelihood_opt",
+        sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
+
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_periodic_dist",
+        sources=["dadapy/cython_/cython_periodic_dist.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
+
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_density",
+        sources=["dadapy/cython_/cython_density.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
 setup(
     name="dadapy",
     packages=["dadapy", "dadapy.utils_"],
     install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
     cmdclass=cmdclass,
-    ext_modules = ext_modules,
+    ext_modules=ext_modules,
 )
 
 
-#OLD VERSION
-#
-#
+
+#COMPILE FROM CYTHON WORKING
 # from setuptools import setup, Extension
+# from Cython.Build import cythonize
 #
 # cmdclass = {}
-# ext_modules = []
-#
-#
-# ### COMPILE FROM C ###
-#
-#
-# class get_numpy_include(object):
-#     """Defer numpy.get_include() until after numpy is installed.
-#     From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-#     """
-#
-#     def __str__(self):
-#         import numpy
-#
-#         return numpy.get_include()
-#
-#
-# # ext_modules += [Extension("dadapy.cython_.cython_functions", sources=["dadapy/cython_/cython_functions.c"],
-# #                           include_dirs=[get_numpy_include()])]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_clustering",
-#         sources=["dadapy/cython_/cython_clustering.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_grads",
-#         sources=["dadapy/cython_/cython_grads.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_maximum_likelihood_opt",
-#         sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_periodic_dist",
-#         sources=["dadapy/cython_/cython_periodic_dist.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_density",
-#         sources=["dadapy/cython_/cython_density.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
+# ext_modules = cythonize("dadapy/cython_/*.pyx")
 #
 # setup(
 #     name="dadapy",
 #     packages=["dadapy", "dadapy.utils_"],
 #     install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
 #     cmdclass=cmdclass,
-#     ext_modules=ext_modules,
+#     ext_modules = ext_modules,
 # )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### COMPILE FROM CYTHON ### NOT WORKING
+
+
+
+
+
 
 
 # exts = [Extension(name='dadapy.cython_functions',
