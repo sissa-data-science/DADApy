21c4590a71308dc5f27211e5466ecfbb15882e0b
Aldo <ealdoglielmo@gmail.com>
2022-01-07 12:42:24 +0100

    optional cython usage in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,67 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
+
 
 cmdclass = {}
-ext_modules = cythonize("dadapy/cython_/*.pyx")
+
+try:
+    from Cython.Build import cythonize
+
+    ext_modules = cythonize("dadapy/cython_/*.pyx")
+except:
+    from setuptools import Extension
+
+    class get_numpy_include(object):
+        """Defer numpy.get_include() until after numpy is installed.
+        From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+        """
+
+        def __str__(self):
+            import numpy
+
+            return numpy.get_include()
+
+    ext_modules = []
+
+    ext_modules += [
+        Extension(
+            "dadapy.cython_.cython_clustering",
+            sources=["dadapy/cython_/cython_clustering.c"],
+            include_dirs=[get_numpy_include()],
+        )
+    ]
+
+    ext_modules += [
+        Extension(
+            "dadapy.cython_.cython_grads",
+            sources=["dadapy/cython_/cython_grads.c"],
+            include_dirs=[get_numpy_include()],
+        )
+    ]
+
+    ext_modules += [
+        Extension(
+            "dadapy.cython_.cython_maximum_likelihood_opt",
+            sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
+            include_dirs=[get_numpy_include()],
+        )
+    ]
+
+    ext_modules += [
+        Extension(
+            "dadapy.cython_.cython_periodic_dist",
+            sources=["dadapy/cython_/cython_periodic_dist.c"],
+            include_dirs=[get_numpy_include()],
+        )
+    ]
+
+    ext_modules += [
+        Extension(
+            "dadapy.cython_.cython_density",
+            sources=["dadapy/cython_/cython_density.c"],
+            include_dirs=[get_numpy_include()],
+        )
+    ]
+
 
 setup(
     name="dadapy",
@@ -10,11 +69,11 @@ setup(
     description="A Python package for Distance-based Analysis of DAta-manifolds.",
     long_description="A Python package for Distance-based Analysis of DAta-manifolds.",
     packages=["dadapy", "dadapy.utils_"],
-    #dependencies
-    #install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
-    install_requires=["cython", "numpy", "scipy", "scikit-learn", "pytest"],
+    # dependencies
+    # install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
+    install_requires=["numpy", "scipy", "scikit-learn", "pytest"],
     cmdclass=cmdclass,
-    ext_modules = ext_modules,
+    ext_modules=ext_modules,
 )
 
 
