9401d1845dd13c8274c9d4c808659bd7e0c3c562
Aldo <ealdoglielmo@gmail.com>
2022-01-19 10:24:33 +0100

    added logo to readthedocs documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,10 +65,15 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-# html_static_path = ["_static"]
-html_static_path = (
-    []
-)  # _static is included by default and this eliminates an annoying warning
+html_static_path = ["_static"]
+#html_static_path = ([])  # _static is included by default and this eliminates an annoying warning
+
+html_logo = "_static/logo_1_vertical_transparent_v2.png"
+
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
 
 autodoc_mock_imports = [
     "dadapy.cython_.cython_clustering",
