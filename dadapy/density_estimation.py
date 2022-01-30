fixed a normalization bug in the "return_interpolated_density" methods
the density was normalised according to the points in X_new, while it should be normalized according to the number of points in X

dadapy/density_estimation.py
@@ -25,7 +25,10 @@ from dadapy.cython_ import cython_density as cd
 from dadapy.cython_ import cython_grads as cgr
 from dadapy.cython_ import cython_maximum_likelihood_opt as cml
 from dadapy.id_estimation import IdEstimation
-from dadapy.utils_.density_estimation import return_density_kstarNN, return_density_PAk
+from dadapy.utils_.density_estimation import (
+    return_not_normalised_density_kstarNN,
+    return_not_normalised_density_PAk,
+)
 from dadapy.utils_.utils import compute_cross_nn_distances
 
 cores = multiprocessing.cpu_count()
@@ -1326,9 +1329,12 @@ class DensityEstimation(IdEstimation):
 
         kstar = np.ones(X_new.shape[0], dtype=int) * k
 
-        log_den, log_den_err, dc = return_density_kstarNN(
+        log_den, log_den_err, dc = return_not_normalised_density_kstarNN(
             cross_distances, self.intrinsic_dim, kstar, interpolation=True
         )
+
+        # Normalise density
+        log_den -= np.log(self.N)
 
         return log_den, log_den_err
 
@@ -1358,9 +1364,12 @@ class DensityEstimation(IdEstimation):
             self.distances,
         )
 
-        log_den, log_den_err, dc = return_density_kstarNN(
+        log_den, log_den_err, dc = return_not_normalised_density_kstarNN(
             cross_distances, self.intrinsic_dim, kstar, interpolation=True
         )
+
+        # Normalise density
+        log_den -= np.log(self.N)
 
         return log_den, log_den_err
 
@@ -1393,9 +1402,12 @@ class DensityEstimation(IdEstimation):
             self.distances,
         )
 
-        log_den, log_den_err, dc = return_density_PAk(
+        log_den, log_den_err, dc = return_not_normalised_density_PAk(
             cross_distances, self.intrinsic_dim, kstar, self.maxk, interpolation=True
         )
+
+        # Normalise density
+        log_den -= np.log(self.N)
 
         return log_den, log_den_err
 

dadapy/utils_/density_estimation.py
@@ -19,7 +19,9 @@ from scipy.special import gammaln
 from dadapy.cython_ import cython_maximum_likelihood_opt as cml
 
 
-def return_density_kstarNN(distances, intrinsic_dim, kstar, interpolation=False):
+def return_not_normalised_density_kstarNN(
+    distances, intrinsic_dim, kstar, interpolation=False
+):
     N = distances.shape[0]
     dc = np.zeros(N, dtype=float)
     prefactor = np.exp(
@@ -30,6 +32,7 @@ def return_density_kstarNN(distances, intrinsic_dim, kstar, interpolation=False)
     if not interpolation:
         log_den = np.log(kstar, dtype=float)
         log_den_err = 1.0 / np.sqrt(kstar, dtype=float)
+
     else:
         log_den = np.log(kstar - 1, dtype=float)
         log_den_err = 1.0 / np.sqrt(kstar - 1, dtype=float)
@@ -52,7 +55,9 @@ def return_density_kstarNN(distances, intrinsic_dim, kstar, interpolation=False)
     return log_den, log_den_err, dc
 
 
-def return_density_PAk(distances, intrinsic_dim, kstar, maxk, interpolation=False):
+def return_not_normalised_density_PAk(
+    distances, intrinsic_dim, kstar, maxk, interpolation=False
+):
     N = distances.shape[0]
 
     dc = np.zeros(N, dtype=float)
@@ -96,10 +101,5 @@ def return_density_PAk(distances, intrinsic_dim, kstar, maxk, interpolation=Fals
             log_den[i] = rr
         if log_den[i] < log_den_min:
             log_den_min = log_den[i]
-
-    # Normalise density
-    log_den -= np.log(N)
-    log_den = log_den
-    log_den_err = log_den_err
 
     return log_den, log_den_err, dc