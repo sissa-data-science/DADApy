91df3a3725f04025e502518c7b38b08283061ef8
Aldo <ealdoglielmo@gmail.com>
2022-02-13 17:41:23 +0100

    removed unstable Cython installation from main branch

--- setup.py
+++ setup.py
@@ -1,66 +1,58 @@
 from setuptools import setup, Extension
 
-
 cmdclass = {}
 
-try:
-    from Cython.Build import cythonize
+class get_numpy_include(object):
+    """Defer numpy.get_include() until after numpy is installed.
+    From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+    """
 
-    ext_modules = cythonize("dadapy/cython_/*.pyx")
-except:
-    from setuptools import Extension
+    def __str__(self):
+        import numpy
 
-    class get_numpy_include(object):
-        """Defer numpy.get_include() until after numpy is installed.
-        From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-        """
+        return numpy.get_include()
 
-        def __str__(self):
-            import numpy
+ext_modules = []
 
-            return numpy.get_include()
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_clustering",
+        sources=["dadapy/cython_/cython_clustering.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
-    ext_modules = []
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_grads",
+        sources=["dadapy/cython_/cython_grads.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
-    ext_modules += [
-        Extension(
-            "dadapy.cython_.cython_clustering",
-            sources=["dadapy/cython_/cython_clustering.c"],
-            include_dirs=[get_numpy_include()],
-        )
-    ]
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_maximum_likelihood_opt",
+        sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
-    ext_modules += [
-        Extension(
-            "dadapy.cython_.cython_grads",
-            sources=["dadapy/cython_/cython_grads.c"],
-            include_dirs=[get_numpy_include()],
-        )
-    ]
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_periodic_dist",
+        sources=["dadapy/cython_/cython_periodic_dist.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
-    ext_modules += [
-        Extension(
-            "dadapy.cython_.cython_maximum_likelihood_opt",
-            sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
-            include_dirs=[get_numpy_include()],
-        )
-    ]
-
-    ext_modules += [
-        Extension(
-            "dadapy.cython_.cython_periodic_dist",
-            sources=["dadapy/cython_/cython_periodic_dist.c"],
-            include_dirs=[get_numpy_include()],
-        )
-    ]
-
-    ext_modules += [
-        Extension(
-            "dadapy.cython_.cython_density",
-            sources=["dadapy/cython_/cython_density.c"],
-            include_dirs=[get_numpy_include()],
-        )
-    ]
+ext_modules += [
+    Extension(
+        "dadapy.cython_.cython_density",
+        sources=["dadapy/cython_/cython_density.c"],
+        include_dirs=[get_numpy_include()],
+    )
+]
 
 
 setup(
@@ -69,94 +61,25 @@ setup(
     description="A Python package for Distance-based Analysis of DAta-manifolds.",
     long_description="A Python package for Distance-based Analysis of DAta-manifolds.",
     packages=["dadapy", "dadapy.utils_"],
-    # dependencies
-    # install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
     install_requires=["numpy", "scipy", "scikit-learn", "matplotlib"],
     extras_require={"dev": ["tox", "black", "isort", "pytest"]},
     cmdclass=cmdclass,
     ext_modules=ext_modules,
 )
 
-
-# COMPILE FROM CYTHON WORKING
-# from setuptools import setup, Extension
+### COMPILE FROM CYTHON ### NOT WORKING
+#
 # from Cython.Build import cythonize
-#
-#
-#
-# ### COMPILE FROM C ###
-# class get_numpy_include(object):
-#     """Defer numpy.get_include() until after numpy is installed.
-#     From: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-#     """
-#
-#     def __str__(self):
-#         import numpy
-#
-#         return numpy.get_include()
-#
-#
-# # ext_modules += [Extension("dadapy.cython_.cython_functions", sources=["dadapy/cython_/cython_functions.c"],
-# #                           include_dirs=[get_numpy_include()])]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_clustering",
-#         sources=["dadapy/cython_/cython_clustering.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_grads",
-#         sources=["dadapy/cython_/cython_grads.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_maximum_likelihood_opt",
-#         sources=["dadapy/cython_/cython_maximum_likelihood_opt.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_periodic_dist",
-#         sources=["dadapy/cython_/cython_periodic_dist.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-#
-# ext_modules += [
-#     Extension(
-#         "dadapy.cython_.cython_density",
-#         sources=["dadapy/cython_/cython_density.c"],
-#         include_dirs=[get_numpy_include()],
-#     )
-# ]
-
+# ext_modules = cythonize("dadapy/cython_/*.pyx")
 #
 # setup(
 #     name="dadapy",
+#     url="https://dadapy.readthedocs.io/",
+#     description="A Python package for Distance-based Analysis of DAta-manifolds.",
+#     long_description="A Python package for Distance-based Analysis of DAta-manifolds.",
 #     packages=["dadapy", "dadapy.utils_"],
-#     #dependencies
-#     install_requires=["numpy", "scipy", "scikit-learn", "Cython", "pytest"],
+#     install_requires=["numpy", "scipy", "scikit-learn", "matplotlib"],
+#     extras_require={"dev": ["tox", "black", "isort", "pytest"]},
 #     cmdclass=cmdclass,
 #     ext_modules=ext_modules,
 # )
-
-
-### COMPILE FROM CYTHON ### NOT WORKING
-
-# exts = [Extension(name='dadapy.cython_functions',
-#                   sources=["cython_/cython_functions.pyx", "cython_/cython_functions.c"],
-#                   include_dirs=[numpy.get_include()])]
-
-# setup(name='dadapy', packages=['dadapy'],
-#       install_requires=['numpy', 'scipy', 'scikit-learn'],
-#       cmdclass=cmdclass,
-#       ext_modules=cythonize(exts))
