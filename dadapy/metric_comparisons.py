3456a39a338366d3890d7d0a1faa933ffbf73b83
DiegoD <doimo.diego@gmail.com>
2022-04-28 12:37:36 +0200

    improvement/bugfix overlap

--- dadapy/metric_comparisons.py
+++ dadapy/metric_comparisons.py
@@ -528,21 +528,21 @@ class MetricComparisons(Base):
         X2_ = self.X[:, coords2]
 
         _, dist_indices1_ = compute_nn_distances(
-            X1_, self.maxk, self.metric, self.period
+            X1_, k+2, self.metric, self.period
         )
         _, dist_indices2_ = compute_nn_distances(
-            X2_, self.maxk, self.metric, self.period
+            X2_, k+2, self.metric, self.period
         )
 
-        overlaps = []
-        for i in range(self.N):
-
-            overlap_i = (
-                sum(dist_indices1_[i, 1 : k + 1] == dist_indices2_[i, 1 : k + 1]) / k
-            )
-            overlaps.append(overlap_i)
-
-        overlap = np.mean(overlaps)
+        overlap = np.mean(dist_indices1_[:, 1: k+1]==dist_indices[:, 1: k+1])
+        #overlaps = []
+        # for i in range(self.N):
+        #
+        #     overlap_i = (
+        #         sum(dist_indices1_[i, 1 : k + 1] == dist_indices2_[i, 1 : k + 1]) / k
+        #     )
+        #     overlaps.append(overlap_i)
+        #overlap = np.mean(overlaps)
 
         return overlap
 
