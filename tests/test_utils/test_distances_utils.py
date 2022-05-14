8f0ae6868d6763695453743cba85e73804d262b3
DiegoD <doimo.diego@gmail.com>
2022-05-14 09:47:08 +0200

    solve linting errors

--- tests/test_utils/test_distances_utils.py
+++ tests/test_utils/test_distances_utils.py
@@ -2,7 +2,8 @@ from dadapy._utils import utils
 import numpy as np
 import pytest
 
+
 def test_zero_dist():
-    X = np.array([[0., 0., 0.], [0., 0., 0.], [0.9, 0., 0.]])
+    X = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.9, 0.0, 0.0]])
     with pytest.warns(UserWarning):
         utils.compute_nn_distances(X, maxk=2, metric="euclidean", period=None)
