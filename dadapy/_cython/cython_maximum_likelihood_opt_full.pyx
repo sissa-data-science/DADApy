4188631e6f7f6bc181ee64e9037a722f22d4df6e
DiegoD <doimo.diego@gmail.com>
2022-07-19 09:44:36 +0200

    small improvements optimized pak

--- dadapy/_cython/cython_maximum_likelihood_opt_full.pyx
+++ dadapy/_cython/cython_maximum_likelihood_opt_full.pyx
@@ -21,28 +21,28 @@ def _nrmaxl(np.ndarray[floatTYPE_t, ndim = 1] F,
 
     # declarations
     cdef DTYPE_t                                i, j, niter, flag
-    cdef floatTYPE_t                            a, stepmax, lr, grad_a, delta_a, grad_f, delta_f, gf_tmp, l, func
+    cdef floatTYPE_t                            a, stepmax, lr, grad_a, delta_a, grad_f, delta_f, gf_tmp, l, func, detinv
     cdef floatTYPE_t                            fepsilon    = np.finfo(float).eps
     cdef np.ndarray[floatTYPE_t, ndim = 2]      Hess        = np.zeros((2, 2))
     cdef np.ndarray[floatTYPE_t, ndim = 2]      HessInv     = np.zeros((2, 2))
 
 
     #Helper funcion computes the inverse Hessian (2x2)
-    def invert(np.ndarray[floatTYPE_t, ndim = 2] A, np.ndarray[floatTYPE_t, ndim = 2] B):
-        cdef floatTYPE_t detinv
-
-        detinv = 1./(A[0,0]*A[1,1] - A[0,1]*A[1,0])
-        B[0,0] = +detinv * A[1,1]
-        B[1,0] = -detinv * A[1,0]
-        B[0,1] = -detinv * A[0,1]
-        B[1,1] = +detinv * A[0,0]
+    # def invert(np.ndarray[floatTYPE_t, ndim = 2] A, np.ndarray[floatTYPE_t, ndim = 2] B):
+    #     cdef floatTYPE_t detinv
+    #
+    #     detinv = 1./(A[0,0]*A[1,1] - A[0,1]*A[1,0])
+    #     B[0,0] = +detinv * A[1,1]
+    #     B[1,0] = -detinv * A[1,0]
+    #     B[0,1] = -detinv * A[0,1]
+    #     B[1,1] = +detinv * A[0,0]
 
     N = F.shape[0]
     for i in range(N):
 
         flag = 0
-        for vol in volumes[i]:
-            if vol<1.0e-300:
+        for j in range(kstar[i]):
+            if volumes[i, j] < 1.0e-300:
                 flag = 1
                 break
 
@@ -73,7 +73,12 @@ def _nrmaxl(np.ndarray[floatTYPE_t, ndim = 1] F,
                     Hess[1,1] = Hess[1,1] - l**2*gf_tmp
                 Hess[1,0] = Hess[0,1]
 
-                invert(Hess, HessInv)
+                #inversion of the hessian matrix
+                detinv = 1./(Hess[0,0]*Hess[1,1] - Hess[0,1]*Hess[1,0])
+                HessInv[0,0] = +detinv * Hess[1,1]
+                HessInv[1,0] = -detinv * Hess[1,0]
+                HessInv[0,1] = -detinv * Hess[0,1]
+                HessInv[1,1] = +detinv * Hess[0,0]
 
                 #parameter step calculation
                 delta_f = (HessInv[0,0]*grad_f+HessInv[0,1]*grad_a)
@@ -89,7 +94,6 @@ def _nrmaxl(np.ndarray[floatTYPE_t, ndim = 1] F,
                 F[i] = F[i] - lr*delta_f
                 a = a - lr*delta_a
 
-                #
                 if ((abs(a) <= fepsilon ) or (abs(F[i]) <= fepsilon )):
                     func = max(abs(grad_f),abs(grad_a))
                 else:
