387b546abe4d1c4bcd186788f710d706ad36c468
imacocco <imacocco@sissa.it>
2023-05-15 11:03:59 +0200

    first commit

--- dadapy/data.py
+++ dadapy/data.py
@@ -77,7 +77,7 @@ class Data(Clustering, MetricComparisons):
             n_iter: number of iteration
             Dthr: threshold value for the kstar test
             d0: minimum id value
-            d1: maxumum id value
+            d1: maximum id value
             eps: threshold for the convergence of the Gride algorithm
 
         Returns:
@@ -86,6 +86,79 @@ class Data(Clustering, MetricComparisons):
         # start with an initial estimate of the ID
         if initial_id is None:
             self.compute_id_2NN()
+        else:
+            self.compute_distances()
+            self.set_id(initial_id)
+
+        ids = []
+        ids_err = []
+        kstars = []
+        log_likelihoods = []
+
+        for i in range(n_iter):
+            # compute kstar
+            self.compute_kstar(Dthr)
+            print("iteration ", i)
+            print("id ", self.intrinsic_dim)
+
+            # compute n2 and n1 via kstar. If not even, make it even by adding one
+            n2s = self.kstar
+            not_even = n2s % 2 != 0
+            n2s[not_even] = n2s[not_even] + 1
+            assert sum(n2s % 2 != 0) == 0
+            n1s = (n2s / 2).astype(int)
+
+            # compute the mus
+            mus = np.array(
+                [
+                    self.distances[i, n2] / self.distances[i, n1]
+                    for i, (n1, n2) in enumerate(zip(n1s, n2s))
+                ]
+            )
+            # compute the id using Gride
+            id, id_err = self._compute_id_gride_single(d0, d1, mus, n1s, n2s, eps)
+            self.set_id(id)
+            log_lik = -ut._neg_loglik(self.dtype, id, mus, n1s, n2s)
+
+            ids.append(id)
+            ids_err.append(id_err)
+            kstars.append(self.kstar)
+            log_likelihoods.append(log_lik)
+
+        ids = np.array(ids)
+        ids_err = np.array(ids_err)
+        kstars = np.array(kstars)
+        log_likelihoods = np.array(log_likelihoods)
+
+        id_scale = 0.0
+        for i, (n1, n2) in enumerate(zip(n1s, n2s)):
+            id_scale += self.distances[i, n1]
+            id_scale += self.distances[i, n2]
+        id_scale /= 2 * self.N
+
+        self.intrinsic_dim = id
+        self.intrinsic_dim_err = id_err
+        self.intrinsic_dim_scale = id_scale
+
+        return ids, ids_err, kstars, log_likelihoods
+
+
+    def return_ids_kstar_binomial(
+        self, initial_id=None, n_iter=5, Dthr=23.92812698, r=0.5#, d1=1000, eps=1e-7
+    ):
+        """Return the id estimates of the binomial algorithm coupled with the kstar estimation of the scale.
+
+        Args:
+            initial_id (float): initial estimate of the id default uses 2NN
+            n_iter (int): number of iteration
+            Dthr (float): threshold value for the kstar test
+            r (float): parameter of binomial estimator, 0 < r < 1
+        Returns:
+            ids, ids_err, kstars, log_likelihoods
+        """
+        # start with an initial estimate of the ID
+        if initial_id is None:
+            self.compute_id_binomial_k(k=10, r=r)
         else:
             self.compute_distances()
             self.set_id(initial_id)
