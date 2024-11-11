e312f886910c586099d9529f4944f0eee319de4a
vdeltatto <vdeltatt@sissa.it>
2024-11-11 15:10:28 +0100

    Add conditional dependencies for Python 3.8

--- dadapy/__init__.py
+++ dadapy/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from ._utils.utils import *
 from .base import Base
 from .clustering import Clustering
@@ -5,10 +7,12 @@ from .data import Data
 from .data_sets import DataSets
 from .density_advanced import DensityAdvanced
 from .density_estimation import DensityEstimation
-from .diff_imbalance import DiffImbalance
 from .feature_weighting import FeatureWeighting
 from .id_discrete import *
 from .id_estimation import IdEstimation
 from .kstar import KStar
 from .metric_comparisons import MetricComparisons
 from .neigh_graph import NeighGraph
+
+if sys.version_info >= (3, 9):
+    from .diff_imbalance import DiffImbalance
