5c733ac4af93ea8b09cf039639d0ff7bc31a890b
diego <doimo.diego@gmail.com>
2026-05-16 15:20:28 +0200

    adding imports of IdDiscrete Hamming BID to __init__.py consistent with other modules

--- dadapy/__init__.py
+++ dadapy/__init__.py
@@ -8,7 +8,7 @@ from .data_sets import DataSets
 from .density_advanced import DensityAdvanced
 from .density_estimation import DensityEstimation
 from .feature_weighting import FeatureWeighting
-from .id_discrete import *
+from .id_discrete import IdDiscrete
 from .id_estimation import IdEstimation
 from .kstar import KStar
 from .metric_comparisons import MetricComparisons
@@ -16,5 +16,5 @@ from .neigh_graph import NeighGraph
 
 if sys.version_info >= (3, 9):
     from .diff_imbalance import DiffImbalance
-if sys.version_info >= (3, 9):
     from .causal_graph import CausalGraph
+    from .hamming import BID, Hamming
